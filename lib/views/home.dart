4b02fb80addaae19d1a42c3fad4398febeaf18d6
Suvoo <suvodeepsinha1981@gmail.com>
2020-03-28 18:21:13 +0530

    updated forward news menu

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:newsapp/helper/data.dart';
@@ -34,9 +35,18 @@ class _HomeState extends State<Home> {
         child: Column(
           children: <Widget>[
             Container(
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              height: 70,
               child: ListView.builder(
                   itemCount: categories.length,
-                  itemBuilder: null
+                  shrinkWrap: true,
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (context,index){
+                    return CategoryTile(
+                      imageUrl: categories[index].imageUrl,
+                      categoryName: categories[index].categoryName,
+                    );
+                  }
               ),
             )
           ],
@@ -52,13 +62,48 @@ class CategoryTile extends StatelessWidget {
   CategoryTile({this.imageUrl,this.categoryName});
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Stack(
-        children: <Widget>[
-          Image.network(imageUrl,width: 120,height: 60,),
-        ],
+    return GestureDetector(
+      onTap: (){
+        
+      },
+      child: Container(
+        margin: EdgeInsets.only(right: 16),
+        child: Stack(
+          children: <Widget>[
+            ClipRRect(
+                borderRadius: BorderRadius.circular(6),
+                child: Image.network(imageUrl,width: 120,height: 60,fit: BoxFit.cover,)),
+            Container(
+              alignment: Alignment.center,
+              width: 120,height: 60,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(6),
+                color: Colors.black26,
+              ),
+              child: Text(categoryName,style: TextStyle(
+                color: Colors.white,
+                fontSize: 14,
+                fontWeight: FontWeight.w500
+              ),),
+            )
+          ],
+        ),
       ),
     );
   }
 }
+
+class BlogTile extends StatelessWidget {
+
+  final String imageUrl,title,desc;
+  BlogTile({@required this.imageUrl,@required this.title,@required this.desc})
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+    );
+  }
+}
+
 
